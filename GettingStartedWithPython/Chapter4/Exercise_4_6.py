5054afc579a0be455be22638aa9b188eb05eb88d
BjornWithDots <bjorn@hotpixel.se>
2022-11-02 15:49:25 +0100

    Cleaned and fixed

--- GettingStartedWithPython/Chapter4/Exercise_4_6.py
+++ GettingStartedWithPython/Chapter4/Exercise_4_6.py
@@ -1,6 +1,20 @@
 def computepay(h, r):
-    return 42.37
+    if h <= 40:
+        pay = h * r
+    else:
+        pay = (h - 40) * (r * 1.5)
+        pay = pay + (40 * r)
+    return pay
 
 hrs = input("Enter Hours:")
-p = computepay(10, 20)
+rate = input("Enter rate:")
+
+try:
+    fhrs = float(hrs)
+    frate = float(rate)
+except:
+    print("No number entered")
+    exit()
+
+p = computepay(fhrs, frate)
 print("Pay", p)
