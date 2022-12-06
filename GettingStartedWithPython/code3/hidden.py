d809944e2832198a8036a7d993592ad1a8ba07cb
Björn Blomberg <bjorn.blomberg@netonnet.com>
2022-12-06 15:59:21 +0100

    Prepare ch 16

--- GettingStartedWithPython/code3/hidden.py
+++ GettingStartedWithPython/code3/hidden.py
@@ -3,12 +3,15 @@
 # https://apps.twitter.com/
 # Create new App and get the four strings
 
+client_id = 'M2czbEFfZHotZjlaX1FRM21pbEo6MTpjaQ'
+client_secrtet = 'U0bRqpqh4ILq2yb6qCPyAxrwRTMsF7L_OSlCjNDnxxuwCEO2J6'
+
 api_key = 'Kxx29Lhpy5GniSOpWg6AXz5QP'
 api_key_secret = 'SkFc7iF6PRSpjJ0kbQwdVLufP6lK9ltid4A4Yd5hvKdpLUYW4N'
 bearer_token= 'AAAAAAAAAAAAAAAAAAAAAIPnjgEAAAAAf7%2BIiDPgSTz%2BkicLdUbCEQ0K%2B%2B8%3Df45E10JJkzhDChdGeZVANdvhLPOBd3KSjHeXWW1uuZvOBZg7D0'
 
 def oauth():
-    return {"consumer_key": "h7Lu...Ng",
-            "consumer_secret": "dNKenAC3New...mmn7Q",
-            "token_key": "10185562-eibxCp9n2...P4GEQQOSGI",
-            "token_secret": "H0ycCFemmC4wyf1...qoIpBo"}
+    return {"consumer_key": "etuefEvbyNyc46S73rrCgiluS",
+            "consumer_secret": "oJhPUyy4ovEYUnptbEiyxo0KxtHgw99YeN4TXOwtMIwISsFhBa",
+            "token_key": "38412842-Z5bM2KP1Gg5I1YjqkjZsgmiJB1rixtk85aoXlqMRS",
+            "token_secret": "jaIvdCNs1YAj423Vrk7vuBhYf0su1VbdR1QAjZedCu86b"}
